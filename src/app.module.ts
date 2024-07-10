701f65b4d55fa616f58d53a127e6c0cb6153724b
Josh-Savvy <joshsavyy007@gmail.com>
2024-07-10 12:47:56 +0100

    chore: database configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import databaseConfig from './config/database.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { InvoiceModule } from './modules/invoice/invoice.module';
+import databaseConfig, { DatabaseConfig } from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './modules/user/entities/user.entity';
+import Client from './modules/user/entities/client.entity';
+import { Invoice } from './modules/invoice/entities/invoice.entity';
+import { Auth } from './modules/auth/entities/auth.entity';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -10,6 +18,24 @@ import databaseConfig from './config/database.config';
       isGlobal: true,
       load: [databaseConfig],
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<DatabaseConfig>) => ({
+        type: 'postgres',
+        host: configService.get('host'),
+        port: +configService.get('port'),
+        username: configService.get('user'),
+        password: configService.get('password'),
+        database: configService.get('database'),
+        entities: [User, Client, Invoice, Auth],
+        synchronize: true,
+        // logging: true,
+      }),
+    }),
+    UserModule,
+    AuthModule,
+    InvoiceModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,7 +1,8 @@
-type DatabaseConfig = {
+export type DatabaseConfig = {
   host: string;
   port: number;
   user: string;
+  password?: string;
   database: string;
 };
 
