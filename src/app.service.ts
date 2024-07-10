7616fb1eb4e7861d23ab2f5fea3df02554bb5095
Josh-Savvy <joshsavyy007@gmail.com>
2024-07-10 12:38:22 +0100

    feat: configurations setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    const date = new Date();
+    return {
+      message: 'Server is healthy!',
+      server_date: `${date.toDateString()} ${date.toTimeString()}`,
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = parseInt(process.env.PORT) || 3000;
+  const logger = new Logger('Nest Application');
+  await app
+    .listen(PORT)
+    .then(() => logger.log(`Nest application is running on PORT: ${PORT}`));
 }
+
 bootstrap();
